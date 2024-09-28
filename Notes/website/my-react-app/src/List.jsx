9357c7938d45c736fe68785faa401fa73a7d3529
woar <imborg@qq.com>
2024-09-28 22:01:48 +0800

    add file chapter6 render lists 2

--- Notes/website/my-react-app/src/List.jsx
+++ Notes/website/my-react-app/src/List.jsx
@@ -1,24 +1,18 @@
 
-function List() {
-    
-    const fruits = [{id: 1,name:"coconut", calories:105},
-                    {id: 2,name:"orange", calories:159},
-                    {id: 3,name:"pineapple", calories:37},
-                    {id: 4,name:"banana", calories:45},
-                    {id: 5,name:"apple", calories:95}];
-    // fruits.sort((a, b) => a.name.localeCompare(b.name)); // ALPHABEFTICAL
-    // fruits.sort((a, b) => b.name.localeCompare(a.name)); // REVERSE ALPHABEFTICAL
-    // fruits.sort((a, b) => a.calories - b.calories); // NUMERIC
-    // fruits.sort((a, b) => b.calories - a.calories); // REVERSE NUMERIC
-    // const lowCalFruits = fruits.filter(fruit => fruit.calories < 100);         
-    const highCalFruits = fruits.filter(fruit => fruit.calories >= 100);         
+function List(props) {
 
-    const listItems = highCalFruits.map(highCalFruit => <li key={highCalFruit.id}>
-                                            {highCalFruit.name}: &nbsp;
-                                            <b>{highCalFruit.calories}</b>
+    const category = props.category;
+    const itemList = props.items;
+
+    const listItems = itemList.map(item => <li key={item.id}>
+                                            {item.name}: &nbsp;
+                                            <b>{item.calories}</b>
                                             </li> )
     
-    return(<ol>{listItems}</ol>);
+    return(<>
+            <h3 className="list-category">{category}</h3>
+            <ol className="list-item">{listItems}</ol>
+            </>);
 }
 
 export default List
