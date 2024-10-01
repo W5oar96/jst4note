3ff18edcf3bf9c6bdba4fb9fec28af1ab5fb16b5
woar <imborg@qq.com>
2024-10-01 23:33:39 +0800

    add file chapter8 useState() hook 3

--- Notes/website/my-react-app/src/MyComponent.jsx
+++ Notes/website/my-react-app/src/MyComponent.jsx
@@ -2,17 +2,35 @@ import React,{useState} from "react";
 
 function MyComponent() {
 
-    const [name, setName] = useState();
+    const [name, setName] = useState("Guest");
+    const [age, setAge] = useState(0);
+    const [isEmpolyed, setIsEmpolyed] = useState(false);
 
     const updateName = () => {
         setName("Spongebob");
         console.log(name);
     }
 
+    const incrementAge = () => {
+        setAge(age + 1);
+        console.log(age);
+    }
+    
+    const toggleIsEmpolyedStatus = () => {
+        setIsEmpolyed(!isEmpolyed);
+        console.log(isEmpolyed);
+    }   
+
     return(
         <div>
             <p>Name: {name}</p>
             <button onClick={updateName}>set name</button>
+
+            <p>Age: {age}</p>
+            <button onClick={incrementAge}>increment age</button>
+
+            <p>IsEmpolyed: {isEmpolyed ? "Yes" : "No"}</p>
+            <button onClick={toggleIsEmpolyedStatus}>toggle isEmpolyed status</button>
         </div>
     )
 }
