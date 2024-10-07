1b1ea644c864153eb79fb5fb98ecc8cdbb9b5cfc
woar <imborg@qq.com>
2024-10-07 21:14:36 +0800

    add file chapter 11 update function

--- Notes/website/my-react-app/src/App.jsx
+++ Notes/website/my-react-app/src/App.jsx
@@ -10,7 +10,7 @@ function App() {
     return(
     <>
       <Card />
-      <ColorPicker />
+      <Counter />
     </>
   )
 }
--- Notes/website/my-react-app/src/Counter.jsx
+++ Notes/website/my-react-app/src/Counter.jsx
@@ -5,15 +5,19 @@ function Counter() {
     const [count, setCount] = useState(0);
 
     const increment = () => {
-        setCount(count + 1);
+        setCount(count => count + 1);
+        setCount(count => count + 1);
+        setCount(count => count + 1);
     }
 
     const decrement = () => {
-        setCount(count - 1);
+        setCount(count => count - 1);
+        setCount(count => count - 1);
+        setCount(count => count - 1);
     }
 
     const reset = () => {
-        setCount(0);
+        setCount(c => c = 0);
     }
 
     return(
