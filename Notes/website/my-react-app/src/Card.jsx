a37d9a49759bb76fa1fa94a5051a04417aab4dc7
cuibo <cuibo@sinochx.com>
2024-11-12 13:56:17 +0800

    change the background-color of Card

--- Notes/website/my-react-app/src/Card.jsx
+++ Notes/website/my-react-app/src/Card.jsx
@@ -1,12 +1,13 @@
 import ProfilePic from './assets/profile.jpg';
 import ProfilePic2 from './assets/profile2.jpg';
-
+import ProfileCat from './assets/profile_cat.jpg';
 
 function Card() {
     return(
         <div className='card'>
             <img className='card-img' src={ProfilePic} alt="profile picture"></img>
             <img className='card-img' src={ProfilePic2} alt="profile picture"></img>
+            <img className='card-img' src={ProfileCat} alt="profile picture"></img>
             <h2 className='card-h2'>FK U</h2>
             <p className='card-p'>Fuck you liulei</p>
         </div>
