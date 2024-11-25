827c4ed68e503e54156544a95aa2d72b52e4b363
cuibo <cuibo@sinochx.com>
2024-11-25 18:08:58 +0800

    change the content

--- Notes/website/my-react-app/src/Card.jsx
+++ Notes/website/my-react-app/src/Card.jsx
@@ -1,6 +1,8 @@
 import ProfilePic from './assets/profile.jpg';
 import ProfilePic2 from './assets/profile2.jpg';
 import ProfileCat from './assets/profile_cat.jpg';
+import ProfileCatFace from './assets/catface.jpg';
+import ProfileYep from './assets/yep.jpg';
 
 function Card() {
     return(
@@ -8,6 +10,8 @@ function Card() {
             <img className='card-img' src={ProfilePic} alt="profile picture"></img>
             <img className='card-img' src={ProfilePic2} alt="profile picture"></img>
             <img className='card-img' src={ProfileCat} alt="profile picture"></img>
+            <img className='card-img' src={ProfileCatFace} alt="profile picture"></img>
+            <img className='card-img' src={ProfileYep} alt="profile picture"></img>
             <h2 className='card-h2'>FK U</h2>
             <p className='card-p'>Fuck you liulei</p>
         </div>
