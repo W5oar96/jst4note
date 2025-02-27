92f0dc8abfd3d1602d2f901b23b2754baea1b818
cuibo <cuibo@sinochx.com>
2025-02-27 13:55:29 +0800

    change the content

--- Notes/temp.java
+++ Notes/temp.java
@@ -1,221 +1,75 @@
-import React, { Component } from 'react';
-import { getLoginUrl, getScanAuthState } from '@/services/api';
-import { Form, message } from 'antd';
-import QRCode from 'qrcode.react';
-import { generateUniqueNumber } from '@/utils/utils';
-
-@Form.create()
-class LoginJT extends Component {
-    state = {isExpired : false , qrCodeUrl : '', qrcodeInterval : false, deviceId :'', qrcode:'' };
-
-  componentWillMount() {
-    // 获取验证码
-    this.getLoginLink();
-  }
-
-  // getLoginLink = async () => {
-  //   getLoginUrl()
-  //     .then((res) => {
-  //       console.log('获取二维码link～～～', res);
-  //       if (res.data && res.data.url) {
-  //         this.setState({
-  //           qrcodeInterval: true,
-  //           qrCodeUrl: res.data.url,
-  //           deviceId: res.data.deviceId,
-  //         })
-  //       } else {
-  //         this.setState({
-  //           qrcodeInterval: false,
-  //         })
-  //         message.error('获取二维码链接失败');
-  //       }
-  //     })
-  //     .catch((err) => {
-  //       this.setState({
-  //         qrcodeInterval: false,
-  //       })
-  //       message.error('获取二维码链接失败');
-  //     });
-  // };
-
-  getLoginLink = async () => {
-    try {
-      const res = await getLoginUrl();
-      console.log('获取二维码link～～～', res);
-      if (res && res.data && res.data.url) {
-        this.setState({
-          qrcodeInterval: true,
-          qrCodeUrl: res.data.url,
-          deviceId: res.data.deviceId
-        });
-      } else {
-        this.setState({ qrcodeInterval: false });
-        message.error('获取二维码链接失败');
-      }
-    } catch (err) {
-      this.setState({ qrcodeInterval: false });
-      message.error('获取二维码链接失败');
-    }
-  };
-
-    getScanUrlStatus = async () => {
-    try {
-      console.log('元神定时启动');
-      // 获取二维码状态
-      const response = await getScanAuthState(param);
-      if (response.flg === 'success' && response.data) {
-        if (response.data.state === '1') {
-          console.log('已扫码未登录');
-          setQrcode(response.data.state);
-        } else if (response.data.state === '2') {
-          console.log('已登录');
-          clearInterval(interval);
-          clearTimeout(timeoutId);
-          const param = {
-            username: response.data.userId,
-            code: response.data.userId,
-            token: response.data.token,
-            appid: response.data.appid,
-          };
-          ssoLogin(param);
-        }
-      } else {
-        setQrcode('4');
-        // setIsError(true);
-        clearInterval(interval);
-        clearTimeout(timeoutId); // 清除超时定时器
-      }
-    } catch (error) {
-      console.error('Error fetching data:', error);
-      setQrcode('4');
-      // setIsError(true);
-      clearInterval(interval);
-      clearTimeout(timeoutId); // 清除超时定时器
-    }
-  };
-
-    refreshQrCode = () => {
-      this.setState({
-        isExpired: false,
-        qrcodeInterval: false,
-        qrcode: '4',
-      })
-     this.getLoginLink();
-  };
-
-    render() {
-      const {isExpired, qrCodeUrl, qrcodeInterval, deviceId, qrcode } = this.state;
-      return (
-        <div style={{ display: 'flex', flexDirection: 'column', width: '100%' }}>
-          <div style={{ display: 'flex', justifyContent: 'center' }}>
-            <img
-              src="https://wwcdn.weixin.qq.com/node/wework/images/WWLogo.04d21e9183.svg"
-              style={{ marginRight: '8px' }}
-            ></img>
-            <h2
-              style={{
-                textAlign: 'center',
-                fontSize: '16px',
-                fontStyle: 'normal',
-                fontWeight: '400',
-                lineHeight: '24px',
-                margin: '40px 0',
-              }}
-            >
-              企业微信扫码登录
-            </h2>
-          </div>
-
-          <div style={{ display: 'flex', justifyContent: 'center' }}><QRCode
-              value={qrCodeUrl}
-              size={210}
-              // style={{margin: '0 auto'}}
-              fgColor="#000000"
-              bgColor="#FFFFFF"
-              level="L"
-              renderAs="svg"
-            />
-            {isExpired && (
-              <div
-                style={{
-                  // position: 'absolute',
-                  top: 0,
-                  left: 0,
-                  width: '210px',
-                  height: '210px',
-                  marginLeft: '-210px',
-                  backgroundColor: 'rgba(255, 255, 255, 0.9)', // 白色背景，50% 不透明度
-                  display: 'flex',
-                  flexDirection: 'column',
-                  justifyContent: 'center',
-                  alignItems: 'center',
-                  cursor: 'pointer',
-                  zIndex: 200,
-                }}
-                onClick={refreshQrCode}
-              >
-                {qrcode == '1' || qrcode == '2' ? (
-                  <img
-                    src={sueccess}
-                    width={40}
-                    height={40}
-                    style={{ marginBottom: '10px' }}
-                    alt="加载失败"
-                  />
-                ) : (
-                  <img
-                    src={wariningSvg}
-                    width={40}
-                    height={40}
-                    style={{ marginBottom: '10px' }}
-                    alt="加载失败"
-                  />
-                )}
-
-                <span
-                  style={{
-                    color: '#000', // 文字颜色改为黑色，以便在白色背景下可见
-                    fontSize: '14px',
-                    fontStyle: 'normal',
-                    fontWeight: '400',
-                    lineHeight: '140%',
-                  }}
-                >
-              {qrcode == '1' || qrcode == '2' ? '已扫码' : '二维码已失效'}
-            </span>
-                <span
-                  style={{
-                    color: '#417ce8', // 文字颜色改为黑色，以便在白色背景下可见
-                    fontSize: '14px',
-                    fontStyle: 'normal',
-                    marginTop: '10px',
-                    fontWeight: '400',
-                    lineHeight: '140%',
-                  }}
-                >
-              刷新
-            </span>
-              </div>
-            )}
-          </div>
-          <div
-            style={{
-              textAlign: 'center',
-              fontSize: '14px',
-              fontStyle: 'normal',
-              fontWeight: '400',
-              marginTop: '24px',
-              marginBottom: '59px',
-              lineHeight: '140%',
-              color: 'var(--ww_base_gray_060, rgba(11, 18, 26, .6))',
-            }}
-          >
-            请使用企业微信二维码扫码登录
-          </div>
-        </div>
-      );
-    }
-
-
-}
-export default LoginJT;
+SELECT 
+    (SELECT COUNT(*) 
+     FROM file_info c, lt_course_chapter d 
+     WHERE c.data_cate = 'course' 
+       AND c.created_date > '2024-09-01 00:00:00' 
+       AND a.train_code = c.developer_code 
+       AND c.id = d.file_id) AS "mainDevelopCourseCount",
+    (SELECT COUNT(*) 
+     FROM file_info c, lt_course_chapter d 
+     WHERE c.data_cate = 'course' 
+       AND c.created_date > '2024-09-01 00:00:00' 
+       AND a.train_code IN (c.minor_developer_code) 
+       AND c.id IN (d.file_id)) AS "minDevelopCourseCount",
+    (SELECT COUNT(*) 
+     FROM file_info c, lt_course_chapter d 
+     WHERE c.data_cate = 'case' 
+       AND c.created_date > '2024-09-01 00:00:00' 
+       AND a.train_code = c.developer_code 
+       AND c.id = d.file_id) AS "mainDevelopCaseCount",
+    (SELECT COUNT(*) 
+     FROM file_info c, lt_course_chapter d 
+     WHERE c.data_cate = 'case' 
+       AND c.created_date > '2024-09-01 00:00:00' 
+       AND a.train_code IN (c.minor_developer_code) 
+       AND c.id IN (d.file_id)) AS "minDevelopCaseCount",
+    SUM(CASE 
+        WHEN a.point_behaviour = 'teach-course' AND a.point_type = 'gain' THEN 1
+        WHEN a.point_behaviour = 'teach-course' AND a.point_type = 'consume' THEN -1
+        ELSE 0 
+    END) AS "teachCount",
+    SUM(CASE 
+        WHEN a.point_behaviour = 'develop-course' AND a.point_level = 'BusinessUnit' AND a.point_type = 'gain' THEN 1
+        WHEN a.point_behaviour = 'develop-course' AND a.point_level = 'BusinessUnit' AND a.point_type = 'consume' THEN -1
+        ELSE 0 
+    END) AS "developCourseBusinessUnitCount",
+    SUM(CASE 
+        WHEN a.point_behaviour = 'develop-course' AND a.point_level = 'company' AND a.point_type = 'gain' THEN 1
+        WHEN a.point_behaviour = 'develop-course' AND a.point_level = 'company' AND a.point_type = 'consume' THEN -1
+        ELSE 0 
+    END) AS "developCourseCompanyCount",
+    SUM(CASE 
+        WHEN a.point_behaviour = 'develop-case' AND a.point_level = 'BusinessUnit' AND a.point_type = 'gain' THEN 1
+        WHEN a.point_behaviour = 'develop-case' AND a.point_level = 'BusinessUnit' AND a.point_type = 'consume' THEN -1
+        ELSE 0 
+    END) AS "developCaseBusinessUnitCount",
+    SUM(CASE 
+        WHEN a.point_behaviour = 'develop-case' AND a.point_level = 'company' AND a.point_type = 'gain' THEN 1
+        WHEN a.point_behaviour = 'develop-case' AND a.point_level = 'company' AND a.point_type = 'consume' THEN -1
+        ELSE 0 
+    END) AS "developCaseCompanyCount",
+    SUM(CASE 
+        WHEN a.point_behaviour = 'teach-course' AND a.point_level = 'BusinessUnit' AND a.point_type = 'gain' THEN 1
+        WHEN a.point_behaviour = 'teach-course' AND a.point_level = 'BusinessUnit' AND a.point_type = 'consume' THEN -1
+        ELSE 0 
+    END) AS "teachBusinessUnitCount",
+    SUM(CASE 
+        WHEN a.point_behaviour = 'teach-course' AND a.point_level = 'company' AND a.point_type = 'gain' THEN 1
+        WHEN a.point_behaviour = 'teach-course' AND a.point_level = 'company' AND a.point_type = 'consume' THEN -1
+        ELSE 0 
+    END) AS "teachCompanyCount",
+    COALESCE(SUM(CASE 
+        WHEN a.point_behaviour = 'develop-course' THEN a.price
+        WHEN a.point_behaviour = 'develop-case' THEN a.price
+        WHEN a.point_behaviour = 'teach-course' THEN a.price
+        ELSE 0 
+    END), 0) AS "price",
+    b.name AS "trainName",
+    b.staff_code AS "staffCode",
+    b.manage_com AS "trainManageCom",
+    a.train_code AS "trainCode"
+FROM lt_point_trace a, lt_student_info b
+WHERE a.train_code = b.train_code
+  AND a.point_behaviour IN ('develop-course', 'develop-case', 'teach-course')
+  -- 动态条件部分
+GROUP BY a.train_code, b.name, b.staff_code, b.manage_com;
